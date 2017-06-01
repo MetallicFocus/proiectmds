dfc3204cb726682e858c78804274486e6e3bc120
lidia12345 <lidia_privantu@yahoo.com>
2017-06-01 16:13:28 +0300

    Add files via upload

---
+++ Rucsac.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rucsac;
+
+/**
+ *
+ * @author Iolanda
+ */
+public class Rucsac {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Vector x=new Vector();
+        System.out.printf("Profitul maxim este: %d", x.rucsac());
+        
+    }
+    
+}
